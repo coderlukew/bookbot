718c7342511503574a9047881c8e0cc04eda6514
coderlukew <coderluke@yahoo.com>
2025-05-07 15:35:03 -0400

    Add additional sorting function; refactor print outputs; refactor imports on main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import count_words
-from stats import count_characters
+from stats import count_words, count_characters, sort_char_counts
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
@@ -12,7 +12,14 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = count_words(book)
     charc_count = count_characters(book)
-    print(f"{num_words} words found in the document")
-    print(charc_count)
+    sorted = sort_char_counts(charc_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f" Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in sorted:
+        print(f" '{entry['char']}': {entry['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,14 @@ def count_characters(input):
         char_count[char] = char_count.get(char, 0) + 1
     return char_count
 
+
+def sort_char_counts(char_count_dict):
+    # Convert dictionary to list of dictionaries with "char" and "num" keys
+    char_list = [
+        {"char": char, "num": count}
+        for char, count in char_count_dict.items()
+        if char.isalpha()
+    ]
+    
+    # Sort the list by "num" in descending order
+    return sorted(char_list, key=lambda x: x["num"], reverse=True)
